cd0f850dbbe60b8a3410aafa4fb495d5b60cb3ad
Jadson Ribeiro (Notebook) <jdsntj@gmail.com>
2021-08-04 15:53:27 -0300

    Show videos on modal

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,26 +1,63 @@
+import { useState } from 'react';
+import ReactModal from 'react-modal';
+
 import { Article } from "../../types";
 
 interface CardProps {
-  article: Article
+  article: Article,
 }
 
 import styles from './card.module.css';
 
+const modalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '70%',
+    height: '70%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+};
+
 export function Card({ article }: CardProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.card} key={article.video_id}>
-      <div className={styles.image}>
-        <img src={article.thumb} />
-      </div>
-      <div className={styles.content}>
-        <p className={`${styles.title} ${styles.textMedium}`}>
-          {article.title}
-        </p>
-        <div className={styles.info}>
-          <p className={styles.textMedium}>{article.duration}</p>
-          <p className={`${styles.price} ${styles.textMedium}`}>Free</p>
+    <>
+      <div 
+        className={styles.card} key={article.video_id}
+        onClick={() => setIsOpen(true)}
+      >
+        <div className={styles.image}>
+          <img src={article.thumb} />
+        </div>
+        <div className={styles.content}>
+          <p className={`${styles.title} ${styles.textMedium}`}>
+            {article.title}
+          </p>
+          <div className={styles.info}>
+            <p className={styles.textMedium}>{article.duration}</p>
+            <p className={`${styles.price} ${styles.textMedium}`}>Free</p>
+          </div>
         </div>
       </div>
-    </div>
+      <ReactModal 
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        style={modalStyles}
+      >
+        <iframe 
+          src={`https://www.youtube.com/embed/${article.video_id}`}
+          allowFullScreen
+          style={{ width: '90%', height: '90%' }}
+        ></iframe>
+      </ReactModal>
+    </>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
+import ReactModal from 'react-modal';
+
 import '../styles/global.css';
 import '../styles/responsive.css';
+
+// import '../styles/modal.css';
+
+ReactModal.setAppElement('#__next');
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
